1422df4efa9a3d59c01e9703006edb9070a187d5
jonesarn <59841177+jonesarn@users.noreply.github.com>
2022-01-09 08:32:13 -0600

    Update Files

--- ToDo.js
+++ ToDo.js
@@ -1,25 +1,43 @@
 //Create 
-var i = 0;
-var count = i++;
 
-
+let ArrayItem = [];
+var idx = 0;
 var ToDoItem = document.getElementById("add").onclick = function() {
-        var item = document.getElementById("search-bar").value;
-        console.log(count + item);
-        if (item != null) {
-            document.getElementById("Items").innerHTML = item;
+    var item = document.getElementById("search-bar").value;
+    console.log(item);
+    if (item != null) {
+        //push
+        ArrayItem.push(item)
+            // show array
+            // spread array out
+        var EmptyString = '';
+        for (idx = 0; idx < ArrayItem.length; idx++) {
+            EmptyString += "<li>" + ArrayItem[idx] + "</li>"
+            var pasteItem = document.getElementById("ShowList").innerHTML = EmptyString;
         }
+        console.log(EmptyString);
 
-
+    } else {
+        console.log("No Info");
+        window.prompt("Enter ");
     }
-    /*
-    function CreateItem(params) {
-        if (ToDoItem != null) {
-            document.getElementById("Items").innerHTML = ToDoItems
-        }
+};
 
+
+
+
+
+
+
+
+/*
+function CreateItem(params) {
+    if (ToDoItem != null) {
+        document.getElementById("Items").innerHTML = ToDoItems
     }
-    */
+
+}
+*/
 
 //Read 
 
